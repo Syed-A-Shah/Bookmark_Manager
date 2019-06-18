aa04bfbdd57e83e798e90016ea6517add49896ad
Student <student@makerss-mbp-6.home>
2019-06-18 18:15:33 +0100

    User can view all his bookmarks

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require 'sinatra/base'
-require_relative './lib/bookmark'
+require './lib/bookmark'
 
-class Bookmark < Sinatra::Base
+class BookmarkManager < Sinatra::Base
 
   get '/' do
     "Bookmark Manager"
   end
 
+  get '/bookmarks' do
+     @bookmarks = Bookmark.all
+     erb :bookmarks
+   end
+   
   run! if app_file == $0
 end
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,6 +1,18 @@
-feature 'View bookmarks' do
-  scenario 'visiting the index page' do
-    visit('/')
-    expect(page).to have_content "Bookmark Manager"
+RSpec.feature 'View Bookmarks' do
+
+  feature 'visiting the homepage' do
+    scenario 'user can view the app name' do
+      visit '/'
+      expect(page).to have_content 'Bookmark Manager'
+    end
+  end
+
+  feature 'Viewing bookmarks' do
+    scenario 'A user can see bookmarks' do
+      visit('/bookmarks')
+      expect(page).to have_content "http://www.makersacademy.com"
+      expect(page).to have_content "http://www.destroyallsoftware.com"
+      expect(page).to have_content "http://www.google.com"
+    end
   end
 end
